2c5255dfb3f9c1f24886a086a744b942a6e5987e
Rana-Tej-Singh <17468009+Rana-Tej-Singh@users.noreply.github.com>
2020-05-10 06:25:30 +0530

    Fixed Errors

--- package-install.sh
+++ package-install.sh
@@ -2,6 +2,7 @@
 
 #sudo amazon-linux-extras install ansible2
 #sudo yum install docker -y && sudo systemctl enable --now docker
+# if getting issue of sudo then remove it from the line or install apt-get install sudo
 
 sudo docker image build -t ansible:v1 .
 
